55eca53dfea29fc441e20ca3f99c6c1ecb18f592
fightwithcrayon <contact@andrewthomashill.co.uk>
2018-09-06 08:24:28 +0100

    Handle playlists

--- app/services/spotify.rb
+++ app/services/spotify.rb
@@ -5,15 +5,14 @@ class Spotify
   end
 
   def parse_link url
-    url.remove!('https://').remove!('http://').remove!('open.spotify.com/').split('/')
-    type = url[0]
-    id = url[1]
-    
-    return find_by_id(id, type)
+    url.remove!('https://').remove!('http://').remove!('open.spotify.com/')
+    components = url.split('/')
+    return components unless components[0] === 'user'
+    return [components[2], components[3], components[1]]
   end
 
   def find_by_id (id, type)
-    return false unless ['track', 'artist', 'album', 'playlist', 'user'].includes type
+    return false unless ['track', 'artist', 'album', 'playlist', 'user'].include? type
     return "RSpotify::#{type.upcase_first}".constantize.find(id)
   end
 end
